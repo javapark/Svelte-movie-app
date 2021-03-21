aec34a5191ca3337fe99e0676c2e976c45338e28
javapark <javaparknet@gmail.com>
2021-03-21 15:33:20 +0900

    전역 메뉴 작성 및 라우츠 파라미터

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -2,16 +2,29 @@
   import { link } from "svelte-spa-router";
   import active from "svelte-spa-router/active";
   import Logo from "~/components/Logo.svelte";
+
+  const menus = [
+    { href: "/", name: "Search" },
+    { href: "/movie/tt4520988", name: "Movie" },
+    { href: "/about", name: "About" },
+  ];
 </script>
 
 <header>
-  <Logo/>
-  <a use:link use:active href="/"> Home </a>
-  <a use:link use:active={"/about"} href="/about">About</a>
+  <Logo />
+  <nav>
+    <ul>
+      {#each menus as { href, name } (name)}
+        <li>
+          <a use:link use:active={href} {href}>{name}</a>
+        </li>
+      {/each}
+    </ul>
+  </nav>
 </header>
 
 <style lang="scss">
-    header :global(a.active) {
-        color: red;
-    }
+  header :global(a.active) {
+    color: red;
+  }
 </style>
